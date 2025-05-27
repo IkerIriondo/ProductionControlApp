e4c8fd66062096f9aa76f46e4c5ddc0a76723576
IkerLenena <145427343+IkerLenenaMA@users.noreply.github.com>
2025-05-27 18:51:48 +0200

    Konfigurazioa ta bi class
    Datubasera sartzeko constanteen deklarazioa.

---
+++ src/main/java/com/example/demo/Customer/Customer.java
@@ -0,0 +1,13 @@
+package com.example.demo.Customer;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    private Integer CodCustomer;
+
+    
+}
---
+++ src/main/java/com/example/demo/Supplier/Supplier.java
@@ -0,0 +1,7 @@
+package com.example.demo.Supplier;
+
+public class Supplier {
+
+
+    
+}
